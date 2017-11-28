14455728d524b87c67429d735668273368ec5a8b
aleksandragluch <aleksandragluch@interia.pl>
2017-11-28 20:15:59 +0100

    seeds

--- Gemfile
+++ Gemfile
@@ -9,11 +9,9 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.2'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
@@ -41,11 +39,19 @@ group :development, :test do
   gem 'selenium-webdriver'
   gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
   gem 'jquery-rails'
+  gem 'sass-rails', '~> 5.0'
+  gem 'sqlite3'
 end
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
+end
+
+group :production do
+  gem 'pg'
+  gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
+  gem 'jquery-rails'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.create(name: "Merra", gender: "women", price: 199.0, filters: "round", image_url: "merra.jpg, ")
+
+Product.create(name: "Liski", gender: "Unisex", price: 250.0, filters: "cute", image_url: "czo.jpg, ")
+
+Product.create(name: "Wayfarer", gender: "Unisex", price: 299.0, filters: "lalal", image_url: "wayfarer.jpg, ")
+
+Product.create(name: "Clubmaster", gender: "Unisex", price: 359.99, filters: "none", image_url: "clubmaster.jpg, ")
+
+Product.create(name: "Beige", gender: "Woman", price: 179.0, filters: "none", image_url: "beige.jpg, ")
+
+Product.create(name: "Rave", gender: "unisex", price:159.99, filters: "none", image_url: "avgold.jpg, ")
+
+Product.create(name: "Glassy", gender: "unisex", price:149.99, filters: "none", image_url: "glassy.jpg, ")
+
+Product.create(name: "Fluffy", gender: "unisex", price:99.99, filters: "none", image_url: "fluffy.jpg, ")
+
+Product.create(name: "Panther", gender: "unisex", price:99.99, filters: "none", image_url: "panther.jpg, ")
+
+Product.create(name: "Bluesss", gender: "Man", price:399.99, filters: "woho", image_url: "blues.jpg, ")
+
